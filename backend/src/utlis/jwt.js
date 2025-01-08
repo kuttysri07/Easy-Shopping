a0cc63cea813fdd214c0259cbac29e76dcbdb001
Sriram <sriram29072002@gmail.com>
2025-01-08 18:30:36 +0530

    for error

--- backend/src/utlis/jwt.js
+++ backend/src/utlis/jwt.js
@@ -11,7 +11,7 @@ const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
     httpOnly: true, // Prevent XSS attacks
     sameSite: "strict", // Prevent CSRF attacks
-    secure:  false, // Send cookie only over HTTPS in production
+    secure:  process.env.NODE_ENV === "production",  // Send cookie only over HTTPS in production
   });
 
   console.log("Cookie set successfully");
